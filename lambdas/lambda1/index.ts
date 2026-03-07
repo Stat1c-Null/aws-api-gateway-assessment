5bfa225257702381be9ac8aebff7a028348563b1
Mikita Slabysh <slabysh2015@gmail.com>
2026-03-06 22:21:59 -0500

    api functions test

--- lambdas/lambda1/index.ts
+++ lambdas/lambda1/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello from Lambda 1!");
+async function getRandomJoke() {
+  const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+  const data = await response.json();
+
+  console.log(data.setup);
+  console.log(data.punchline);
+}
--- lambdas/lambda2/lambda_function.py
+++ lambdas/lambda2/lambda_function.py
@@ -1,1 +1,9 @@
-print("start")
+import requests
+
+response = requests.get("http://ip-api.com/json")
+data = response.json()
+
+print("Country:", data["country"])
+print("City:", data["city"])
+print("Latitude:", data["lat"])
+print("Longitude:", data["lon"])
